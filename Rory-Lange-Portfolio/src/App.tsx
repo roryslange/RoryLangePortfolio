1525f602851b6f7ac77de024db28f9ea6a882b19
rory lange <rorystrattonlange@gmail.com>
2024-07-09 22:58:58 -0400

    fixed navbar position, working on routing and styles, about button broken

--- Rory-Lange-Portfolio/src/App.tsx
+++ Rory-Lange-Portfolio/src/App.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import About from './components/About';
 import Nav from './components/Navbar';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import { Route, Routes } from 'react-router-dom'
 
 
 
 function App() {
-  let page
-  
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <About />
+      break
+    case "/Projects":
+      component = <Projects />
+  }
 
   return (
     <>
-      <div className='flex h-screen max-w-screen-2xl bg-background text-secondary'>
-        
+      <div className='flex bg-background text-secondary'>
         <Nav />
-        <About />
+        <div>
+          <Routes>
+            <Route path='/About' element={<About/>} />
+            <Route path='/Contact' element={<Contact/>} />
+            <Route path='/Projects' element={<Projects/>} />
+          </Routes>
+        </div>
         
       </div>
     </>
--- Rory-Lange-Portfolio/src/components/Navbar.tsx
+++ Rory-Lange-Portfolio/src/components/Navbar.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
-    const path = window.location.pathname;
-    
 
 
     return (
-        <nav>
-            <div>
-                <ul className='flex'>
-                    <li>
-                        <a href="/About">About</a>
+        <nav className=''>
+            <div className='sticky top-0 inset-y-0 h-fit w-screen bg-card'>
+                <ul className='flex gap-4 justify-center'>
+                    <li className='px-4 py-2'>
+                        <Link className='rounded-lg w-[15%] bg-primary' to="/About">About</Link>
                     </li>
-                    <li>
-                        <a href="/Projects">Projects</a>
+                    <li className='rounded-lg px-4 py-2 w-[15%] bg-primary'>
+                        <Link className='rounded-lg w-[15%] bg-primary' to="/Projects">Projects</Link>
                     </li>
-                    <li>
-                        <a href="/About">Personal</a>
+                    <li className='rounded-lg px-4 py-2 w-[15%] bg-primary'>
+                        <Link className='rounded-lg w-[15%] bg-primary' to="/About">Personal</Link>
                     </li>
-                    <li>
-                        <a href="/About">Experience</a>
+                    <li className='rounded-lg px-4 py-2 w-[15%] bg-primary'>
+                        <Link className='rounded-lg w-[15%] bg-primary' to="/About">Experience</Link>
                     </li>
-                    <li>
-                        <a href="/Contact">Contact</a>
+                    <li className='rounded-lg px-4 py-2 w-[15%] bg-primary'>
+                        <Link className='rounded-lg w-[15%] bg-primary' to="/Contact">Contact</Link>
                     </li>
                 </ul>
             </div>
