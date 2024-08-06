00ab5f2f3391ca4acbaca16c8df306ad902332bb
Rory Lange <62157625+roryslange@users.noreply.github.com>
2024-08-06 17:56:49 +0000

    edited styles on navbar, changed some placeholder values

--- Rory-Lange-Portfolio/src/components/About.tsx
+++ Rory-Lange-Portfolio/src/components/About.tsx
@@ -3,14 +3,14 @@ export default function About() {
     return(
         <section>
             <div>
-                <h1>
+                <h1 className="text-2xl">
                     Rory Lange
                 </h1>
             </div>
 
             <div>
                 <p>
-                This page will show personal information about me and my interests
+                This page will show an introduction
 
 
 
