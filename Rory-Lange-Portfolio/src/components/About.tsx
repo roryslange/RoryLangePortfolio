7d95eb6d5f1bd41d12acb8cb799da97d685772f5
roryslange <rorystrattonlange@gmail.com>
2024-08-08 15:47:47 -0400

    changed some styles, added socials icons to about.tsx

--- Rory-Lange-Portfolio/src/components/About.tsx
+++ Rory-Lange-Portfolio/src/components/About.tsx
@@ -1,15 +1,42 @@
+import { SocialIcon } from 'react-social-icons'
+
+
 export default function About() {
+
+    const socials = [
+        { network: 'github', url: 'https://github.com/roryslange' },
+        { network: 'linkedin', url: 'https://linkedin.com/in/roryslange' },
+        { network: 'instagram', url: 'https://instagram.com/rory_lange' }
+      ];
+
+    
 
     return(
         <div>
-            <div>
-                <h1 className="text-3xl">
+            <div className='flex flex-grow pb-4 '>
+                <h1 className="flex flex-grow text-5xl">
                     Rory Lange
                 </h1>
+
+                <ul className='flex justify-self-end gap-4'>
+                    <li>
+                        {socials.map((social, index) => (
+                            <SocialIcon 
+                                key={index} 
+                                network={social.network} 
+                                url={social.url} 
+                                bgColor='#E85A4F' 
+                                fgColor='#D8C3A5' 
+                                className='flex ml-2 hover:drop-shadow-xl hover:-translate-y-1 duration-300' 
+                                />
+                            ))}
+                    </li>
+                </ul>
+
                 <br/>
             </div>
 
-            <div className="overflow-auto">
+            <div className="overflow-auto ">
                 
                 <img src='src/assets/roryMotorcycle.jpeg' 
                     alt="Me and my 2016 Honda CB500F (the photo makes me look short)"
@@ -19,22 +46,25 @@ export default function About() {
                 <div>
                     <p>
                     
-                        Welcome to my personal website! I am currently a Senior at Wayne State University in Detroit, MI, and my projected graduation date is December 2024. 
-                        Throughout my education I have familliarized myself with a large varieties of technologies and frameworks. I am a creative software engineer who has 
-                        a strong desire to learn, a passion for programming, and a hardworking mindset.
+                        Welcome to my personal website! I am currently a Senior at Wayne State University in Detroit, MI, and my projected graduation date is December 2024. Throughout my education I have familliarized myself with a large varieties of technologies and frameworks. I am a creative software engineer who has a strong desire to learn, a passion for programming, and a hardworking mindset.
                         
 
                     </p>
+
                     <br/>
 
                     <p>
 
-                        Feel free to explore this website. I wrote this website using React + Vite to extend my knowledge of frontend frameworks. On the Projects tab you can find
-                        links to repositories of other projects I have created and participated in. Feel free to reach out to me in the socials page, I am always interested in learning
-                        about new opportunities and projects that I could take part in.
+                        Feel free to explore this website. I wrote this website using React + Vite to extend my knowledge of frontend frameworks. On the Projects tab you can find links to repositories of other projects I have created and participated in. Feel free to reach out to me in the socials page, I am always interested in learning about new opportunities and projects that I could take part in.
                     
                     </p>
+
+                    <br/>
+
+                    
                 </div>
+
+                
 
 
             </div>
--- Rory-Lange-Portfolio/src/components/Navbar.tsx
+++ Rory-Lange-Portfolio/src/components/Navbar.tsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
 
-    const LinkStyles = 'flex flex-auto rounded-lg items-center justify-center';
+    const LinkStyles = 'flex flex-auto rounded-lg items-center justify-center text-md';
     const LiStyles = 'flex rounded-sm p-2 hover:shadow-xl hover:bg-primary hover:-translate-y-0.5 duration-300';
 
 
     return (
         <nav className='sticky top-0 z-50'>
-            <div className='py-5 w-screen bg-card shadow-xl items-center'>
+            <div className='py-4 bg-card shadow-xl items-center'>
                 <ul className='flex gap-4 justify-center'>
                     <li className={LiStyles}>
                         <Link className={LinkStyles} to="/About">home</Link>
@@ -19,10 +19,11 @@ const Nav = () => {
                     <li className={LiStyles}>
                         <Link className={LinkStyles} to="/Projects">projects</Link>
                     </li>
-                    <li className={LiStyles}>
+                    {/* <li className={LiStyles}>
                         <p className={LinkStyles}>socials</p>
-                    </li>
+                    </li> */}
                 </ul>
+
             </div>
         </nav>
     )
