8a51ccdc9cb9f200a9a3e5dff04205d455a0b34f
roryslange <rorystrattonlange@gmail.com>
2024-08-08 13:06:21 -0400

    fixed about.tsx styles, floated image. Created variables for Navbar.tsx to increase simplicity with code.

--- Rory-Lange-Portfolio/src/components/About.tsx
+++ Rory-Lange-Portfolio/src/components/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
 
     return(
-        <section>
+        <div>
             <div>
                 <h1 className="text-3xl">
                     Rory Lange
@@ -9,23 +9,36 @@ export default function About() {
                 <br/>
             </div>
 
-            <div className="grid grid-cols-2 text-lg">
-                <p className="">
+            <div className="overflow-auto">
                 
-                    Welcome to my personal website! I am currently a Senior at Wayne State University in Detroit, MI, and my projected graduation date is December 2024. 
-                    Throughout my education I have familliarized myself with a large varieties of technologies and frameworks. I am a creative software engineer who has 
-                    a strong desire to learn, a passion for programming, and a hardworking mindset.
-                </p>
+                <img src='src/assets/roryMotorcycle.jpeg' 
+                    alt="Me and my 2016 Honda CB500F (the photo makes me look short)"
+                    height={800} width={400} 
+                    className="max-h-[50%] max-w-[25%] ml-2 float-right shadow-xl" />
+                
+                <div>
+                    <p>
+                    
+                        Welcome to my personal website! I am currently a Senior at Wayne State University in Detroit, MI, and my projected graduation date is December 2024. 
+                        Throughout my education I have familliarized myself with a large varieties of technologies and frameworks. I am a creative software engineer who has 
+                        a strong desire to learn, a passion for programming, and a hardworking mindset.
+                        
 
-                <img src='src/assets/roryMotorcycle.jpeg' height={600} width={300} className=""></img>
-                
-                <p>
-                    Feel free to explore this website. I wrote this website using React + Vite to extend my knowledge of frontend frameworks. On the Projects tab you can find
-                    links to repositories of other projects I have created and participated in. Feel free to reach out to me in the socials page, I am always interested in learning
-                    about new opportunities and projects that I could take part in.
-                </p>
+                    </p>
+                    <br/>
+
+                    <p>
+
+                        Feel free to explore this website. I wrote this website using React + Vite to extend my knowledge of frontend frameworks. On the Projects tab you can find
+                        links to repositories of other projects I have created and participated in. Feel free to reach out to me in the socials page, I am always interested in learning
+                        about new opportunities and projects that I could take part in.
+                    
+                    </p>
+                </div>
+
+
             </div>
 
-        </section>
+        </div>
     )
 }
--- Rory-Lange-Portfolio/src/components/Navbar.tsx
+++ Rory-Lange-Portfolio/src/components/Navbar.tsx
@@ -2,22 +2,25 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
 
+    const LinkStyles = 'flex flex-auto rounded-lg items-center justify-center';
+    const LiStyles = 'flex rounded-sm p-2 hover:shadow-xl hover:bg-primary hover:-translate-y-0.5 duration-300';
+
 
     return (
         <nav className='sticky top-0 z-50'>
             <div className='py-5 w-screen bg-card shadow-xl items-center'>
                 <ul className='flex gap-4 justify-center'>
-                    <li className='flex rounded-sm p-2 hover:shadow-xl hover:bg-primary hover:-translate-y-0.5 duration-300'>
-                        <Link className='flex flex-auto rounded-lg items-center justify-center' to="/About">home</Link>
+                    <li className={LiStyles}>
+                        <Link className={LinkStyles} to="/About">home</Link>
                     </li>
-                    <li className='flex rounded-sm p-2 hover:shadow-xl hover:bg-primary hover:-translate-y-0.5 duration-300'>
-                        <Link className='flex flex-auto rounded-lg items-center justify-center' to="/Timeline">timeline</Link>
+                    <li className={LiStyles}>
+                        <Link className={LinkStyles} to="/Timeline">timeline</Link>
                     </li>
-                    <li className='flex rounded-sm p-2 hover:shadow-xl hover:bg-primary hover:-translate-y-0.5 duration-300'>
-                        <Link className='flex flex-auto rounded-lg items-center justify-center' to="/Projects">projects</Link>
+                    <li className={LiStyles}>
+                        <Link className={LinkStyles} to="/Projects">projects</Link>
                     </li>
-                    <li className='flex rounded-sm p-2 hover:shadow-xl hover:bg-primary hover:-translate-y-0.5 duration-300'>
-                        <p className='flex flex-auto rounded-lg items-center justify-center'>socials</p>
+                    <li className={LiStyles}>
+                        <p className={LinkStyles}>socials</p>
                     </li>
                 </ul>
             </div>
